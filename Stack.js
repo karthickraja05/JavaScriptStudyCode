d5aa6272507bce2d778465b36ee75daf5beaba0d
karthickraja05 <karthickraja.gm@gmail.com>
2022-04-30 21:25:53 +0530

    Updated Code - Karthickraja

--- Stack.js
+++ Stack.js
@@ -7,13 +7,26 @@ var Stack = function(){
 	}
 
 	this.pop = function(){
-		var result = this.storage[this.count];
-		delete this.storage[this.count];
+		var result = this.storage[this.count-1];
+		delete this.storage[this.count-1];
 		this.count--;
 		return result;
 	}
 
-	this.pop = function(){
-		return this.storage[this.count];
+	this.peek = function(){
+		return this.storage[this.count-1];
 	}
 }
+
+var stack1 = new Stack();
+
+
+stack1.push(1);
+stack1.push(5);
+stack1.push(4);
+stack1.push(6);
+stack1.push(90);
+stack1.peek();
+stack1.pop();
+
+console.log(stack1.count);
