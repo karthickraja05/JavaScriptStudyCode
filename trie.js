88093e115520fa11125898ac9db18c887cda9ee0
KarthickRaja <73303027+karthickraja05@users.noreply.github.com>
2022-05-04 15:25:19 +0530

    Update trie.js

--- trie.js
+++ trie.js
@@ -11,7 +11,7 @@ let Node = function(){
 
 let Trie = function(){
 	this.root = new Node();
-	let words = [];
+	this.words = [];
 	this.add = function(input,node=this.root){
 		if(input.length === 0){
 			node.setEnd();
@@ -63,22 +63,81 @@ let Trie = function(){
 			}
 		}
 	}
+
+	this.getWords2 = function(node,root_key){
+		if(node.isEnd()){
+			this.words.push(root_key);
+		}
+		for (const [key, value] of node.keys) {
+			let newTempKey = root_key+key;
+			if(value.keys.size > 0){
+				this.getWords2(value,newTempKey);	
+			}else if(value.isEnd()){
+				this.words.push(newTempKey);
+			}
+		}
+	}
+
+	this.getWords = function(key){
+		let node = this.root;
+		this.words = [];
+		let all = false;
+		for (var i = 0; i < key.length; i++) {
+			if(node.keys.get(key[i])){
+				node = node.keys.get(key[i]);
+			}else{
+				all = true;
+				break;
+			}
+		}
+		
+		if(!all){
+			this.getWords2(node,key);
+		}
+		return this.words;
+	}
+
 }
 
 let trie = new Trie();
 trie.add('ball');
-trie.add('b');
+trie.add('bacc');
 trie.add('bat');
-trie.add('do');
-trie.add('dorm');
-trie.add('dork');
-trie.add('doll');
-trie.add('send');
-trie.add('sense');
-trie.print();
+trie.add('b');
+trie.add('karthickraj');
+// trie.add('do');
+// trie.add('dorm');
+// trie.add('dork');
+// trie.add('doll');
+// trie.add('send');
+// trie.add('sense');
 // console.log(trie.isWord('do'))
 // console.log(trie.isWord('bat'))
 // console.log(trie.isWord('dor'))
-// console.log();
+// console.log(trie);
+// trie.print();
+
+$('#search').on('change',function(e){
+	let search_word = $(this).val();
+	if(search_word == ''){
+		$('#list').html('<li>No Data Found</li>');
+	}else{
+		let appendData = trie.getWords(search_word);
+		if(appendData.length == 0){
+			$('#list').html('<li>No Data Found</li>');	
+		}else{
+			updateLi(appendData);
+		}
+		
+	}
+});
 
 
+
+function updateLi(data){
+	html = '';
+	for (var i = 0; i < data.length; i++) {
+		html += `<li>${data[i]}</li>`;
+	}
+	$('#list').html(html);
+}
